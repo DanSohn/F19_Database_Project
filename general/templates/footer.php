3b31805087ed7944ade42e61de0ec8f3ab54d28a
Daniel Sohn <chiefgustavo000@gmail.com>
2019-11-25 00:25:27 -0700

    Changed footer to always stay on the bottom of page

--- general/templates/footer.php
+++ general/templates/footer.php
@@ -1,6 +1,7 @@
 
- <footer class ="section">
+ <footer class ="section" style="position:absolute;bottom:0;width:100%;">
  	<div class="center grey-text">Copyright 2019 NKG Graphics</div>
  	<div class="center grey-text">Developed by Peter Namkoong, Panagiota Fytopoulou, and Daniel Sohn</h6>
  </footer>
  </body>
+ 
