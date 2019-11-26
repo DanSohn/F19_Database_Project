f81087641097e1b70ace88ce17ea10add67125d7
Peter Namkoong <peternamkoong@gmail.com>
2019-11-25 22:50:48 -0700

    Prettified Viewinvoice and viewOrder. Still ugly. but better.

--- general/invoicestatus.php
+++ general/invoicestatus.php
@@ -12,9 +12,19 @@
 		$result = mysqli_query($conn, $sql);
 
 		//fetch result in array format
-		$status = mysqli_fetch_assoc($result);
+		$invoice = mysqli_fetch_assoc($result);
 
 		mysqli_free_result($result);
+        $orderNumber = $invoice['OrderNumber'];
+        $sql = "SELECT * FROM order_table WHERE OrderNumber=$orderNumber";
+
+        //get the query result
+        $result = mysqli_query($conn, $sql);
+
+        //fetch result in array format
+        $order = mysqli_fetch_assoc($result);
+
+        mysqli_free_result($result);
 		mysqli_close($conn);
 
 	}
@@ -25,10 +35,86 @@
 
 <?php  include('templates/header.php');?>
 <?php  include('config/cookies.php');?>
-<h3>Order Number: <?php echo htmlspecialchars($status['InvoiceNumber']);?></h3>
+<h3>Order Number: <?php echo htmlspecialchars($invoice['InvoiceNumber']);?></h3>
 <div class="row justify-content-center white z-depth-2" style ="width:1040px">
-	<?php if($status): ?>
-		<h4><?php echo htmlspecialchars($status['InvoiceNumber']." ".$status['cost']." ".$status['status']);?></h4>
+	<?php if($invoice): ?>
+        <h5>Created Date:</h5>
+        <h6><?php echo htmlspecialchars($order['CreatedDate']);?></h6>
+
+        <h5>Client Details:</h5>
+        <table class = "table">
+            <thread>
+                <tr>
+                    <th class ="center">Name</th>
+                    <th class ="center">Address</th>
+                    <th class ="center">City</th>
+                    <th class ="center">Postal Code</th>
+                    <th class ="center">Phone Number</th>
+                    <th class ="center">Email</th>
+                </tr>
+            </thread>
+            <tr>
+                <td class = "center"><?php echo htmlspecialchars($user['FName']);?></td>
+                <td class = "center"><?php echo htmlspecialchars($user['Address']);?></td>
+                <td class = "center"><?php echo htmlspecialchars($user['City']);?></td>
+                <td class = "center"><?php echo htmlspecialchars($user['PostalCode']);?></td>
+                <td class = "center"><?php echo htmlspecialchars($user['PhoneNumber']);?></td>
+                <td class = "center"><?php echo htmlspecialchars($user['Email']);?></td>
+            </tr>
+        </table>
+
+        <h5>Company:</h5>
+        <table class = "table">
+            <thread>
+                <tr>
+                    <th class ="center">Name</th>
+                    <th class ="center">Address</th>
+                    <th class ="center">City</th>
+                    <th class ="center">Postal Code</th>
+                    <th class ="center">Phone Number</th>
+                    <th class ="center">Email</th>
+                </tr>
+            </thread>
+            <tr>
+                <td class = "center">NKG Graphics</td>
+                <td class = "center">123 Namkoong Drive SW</td>
+                <td class = "center">Calgary</td>
+                <td class = "center">T3Z 0C9</td>
+                <td class = "center">4039999357</td>
+                <td class = "center">peter@nkggraphics.com</td>
+            </tr>
+        </table>
+
+        <h5>Order Details:</h5>
+        <table class = "table">
+            <thread>
+                <tr>
+                    <th class ="center">Length</th>
+                    <th class ="center">Width</th>
+                    <th class ="center">Quantity</th>
+                    <th class ="center">Total</th>
+                </tr>
+            </thread>
+            <tr>
+                <td class = "center"><?php echo htmlspecialchars($order['length']);?>"</td>
+                <td class = "center"><?php echo htmlspecialchars($order['width']);?>"</td>
+                <td class = "center"><?php echo htmlspecialchars($order['Quantity']);?></td>
+                <td class = "center">$<?php echo htmlspecialchars($order['Cost']);?></td>
+            </tr>
+        </table>
+
+        <h5>Installation?</h5>
+        <h6>Yes or No</h6>
+
+        <h5>Comment Section:</h5>
+        <p>Thank you for your business!</p>
+
+        <h5>Payment Section:</h5>
+        <?php if($invoice['status'] == 'Not Paid'): ?>
+            <a href="settle_invoice.php?InvoiceNumber=<?php echo $invoice['InvoiceNumber']?>" class = "btn btn-info">Settle now</a>
+        <?php else: ?>
+            <h6>Paid</h6>
+        <?php endif; ?>
 	<?php else: ?>
 		<h5>Order not completed, invoice does not exist yet.</h5>
 
--- general/orderstatus.php
+++ general/orderstatus.php
@@ -12,7 +12,14 @@
 		//fetch result in array format
 		$status = mysqli_fetch_assoc($result);
 		mysqli_free_result($result);
-		mysqli_close($conn);
+
+		$msin = $status['M_SIN'];
+		$sql = "SELECT * FROM person_table WHERE SIN = $msin";
+        $result = mysqli_query($conn, $sql);
+        //fetch result in array format
+        $manager = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+        mysqli_close($conn);
 
 
 
@@ -27,9 +34,53 @@
 <!-- footer style padding not added -->
 <h3>Order Number: <?php echo htmlspecialchars($status['OrderNumber']);?></h3>
 <div class="row justify-content-center white z-depth-2" style ="width:1040px">
-
 	<?php if($status): ?>
-		<h4><?php echo htmlspecialchars($status['OrderNumber']." ".$status['Cost']." ".$status['OrderStatus']);?></h4>
+        <h5>Created Date:</h5>
+    <h6><?php echo htmlspecialchars($status['CreatedDate']);?></h6>
+
+    <h5>Order Status:</h5>
+        <h6><?php echo htmlspecialchars($status['OrderStatus']);?></h6><br>
+
+        <h5>Order Details:</h5>
+        <table class = "table">
+            <thread>
+                <tr>
+                    <th class ="center">Length</th>
+                    <th class ="center">Width</th>
+                    <th class ="center">Quantity</th>
+                </tr>
+            </thread>
+                <tr>
+                    <td class = "center"><?php echo htmlspecialchars($status['length']);?>"</td>
+                    <td class = "center"><?php echo htmlspecialchars($status['width']);?>"</td>
+                    <td class = "center"><?php echo htmlspecialchars($status['Quantity']);?></td>
+                </tr>
+        </table>
+        <h5>Design:</h5>
+        <h6>Coming soon to a theatre near you.</h6>
+        <img src="" alt="">
+
+        <h5>Installation?</h5>
+        <h6>Yes or No</h6>
+
+        <h5>Manager On File:</h5>
+        <table class = "table">
+            <thread>
+                <tr>
+                    <th class ="center">First Name</th>
+                    <th class ="center">Last Name</th>
+                    <th class ="center">Email</th>
+                    <th class ="center">Phone Number</th>
+                </tr>
+            </thread>
+            <tr>
+                <td class = "center"><?php echo htmlspecialchars($manager['FName']);?></td>
+                <td class = "center"><?php echo htmlspecialchars($manager['LName']);?></td>
+                <td class = "center"><?php echo htmlspecialchars($manager['Email']);?></td>
+                <td class = "center"><?php echo htmlspecialchars($manager['PhoneNumber']);?></td>
+            </tr>
+        </table>
+
 	<?php else: ?>
 		<h5>No such order exists.</h5>
 
