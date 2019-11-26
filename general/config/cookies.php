9aeec0c703e9eb6eb861e75cbe3ea75908c6806e
Peter Namkoong <peternamkoong@gmail.com>
2019-11-25 19:44:15 -0700

    Added the SQl query of getting the logged in user's info in the cookies.php. can get logged in user's info using $user[<attribute>]

--- general/config/cookies.php
+++ general/config/cookies.php
@@ -1,5 +1,13 @@
-<?php 
+<?php
+include('config/db_connect.php');
  if(!isset($_COOKIE['uname'])){
  	header("location:login.php");
+ }
+ else{
+     $email = mysqli_real_escape_string($conn, $_COOKIE['uname']);
+     $sql = "SELECT * FROM person_table WHERE Email = '$email'";
+     $result = mysqli_query($conn, $sql);
+     $user = mysqli_fetch_array($result);
+     mysqli_close($conn);
  }
  ?>
